b8d5199675edae5fd54ceed3b40c3324a64f74de
saranya <Machavaram.s@northeastern.edu>
2022-12-09 15:16:25 -0500

    Changed Folder structure

--- Intensive_Foster_Care/src/UserInterface/MainJFrame.java
+++ Intensive_Foster_Care/src/UserInterface/MainJFrame.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package Intensive_Foster_Care;
+package UserInterface;
 
 /**
  *
  * @author saran
  */
-public class Intensive_Foster_Care {
+public class MainJFrame {
 
     /**
      * @param args the command line arguments
